125738faf0b98de7010e549a1dc616c772d1abb7
leandro-gft <lpsacchi@gmail.com>
2020-06-17 11:06:53 -0300

    Removendo registros adicionados pelo form

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,17 @@
-var pacientes = document.querySelectorAll(".paciente")
+var pacientes = document.querySelectorAll(".tabela-pacientes");
 
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick",function(event){
+    var alvoEvento = event.target; //neste caso o TD
+    var paiDoAlvo = alvoEvento.parentNode; //neste caso o TR
+    paiDoAlvo.remove();
+})
+
+/*
 pacientes.forEach(function(paciente){
     paciente.addEventListener("dblclick", function(){
         console.log("Fui clicado");
         this.remove(); //this: a quem o evento está atrelado
     })
-});
+});*/
