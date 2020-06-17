6d440d570b46f8090c83649d75602a22e5b8266d
leandro-gft <lpsacchi@gmail.com>
2020-06-17 14:20:47 -0300

    Capturando erro de requisição

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -3,14 +3,23 @@ var botao = document.querySelector("#buscar-paciente");
 botao.addEventListener("click", function () {
     console.log("Buscando pacientes...")
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pcientes");
     xhr.addEventListener("load", function () {
+        if(xhr.status == 200){
+            erro.classList.add("invisivel");
         var resposta = xhr.responseText;
         var pacientes = JSON.parse(resposta);
 
         pacientes.forEach(function(paciente){
             adicionaPacienteNaTabela(paciente);           
         });
+    } else {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+        var erro = document.querySelector("#erro-ajax");
+        erro.classList.remove("invisivel");
+    }
+
 
     });
 
