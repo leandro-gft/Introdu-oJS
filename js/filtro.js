2b7410f0fbd8bcb63bbe7be9f8bfa48dc4ad3b45
leandro-gft <lpsacchi@gmail.com>
2020-06-17 12:18:16 -0300

    Implementado pesquisa letra-a-letra

--- js/filtro.js
+++ js/filtro.js
@@ -6,13 +6,13 @@ campoFiltro.addEventListener("input", function () {
 
     if (this.value.length > 0) {
 
-
         for (let i = 0; i < pacientes.length; i++) {
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i"); //i: case insensitive
 
-            if (nome != this.value) {
+            if (!expressao.test(nome)) {
                 paciente.classList.add("invisivel")
             } else {
                 paciente.classList.remove("invisivel")
