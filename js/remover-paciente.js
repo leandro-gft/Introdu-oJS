0bd79c4a83b455060a49a450d4328ce742f53236
leandro-gft <lpsacchi@gmail.com>
2020-06-17 11:21:24 -0300

    Adicionada animação para remover registros

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,17 +1,14 @@
-var pacientes = document.querySelectorAll(".tabela-pacientes");
-
 var tabela = document.querySelector("table");
 
-tabela.addEventListener("dblclick",function(event){
+tabela.addEventListener("dblclick", function (event) {
     var alvoEvento = event.target; //neste caso o TD
     var paiDoAlvo = alvoEvento.parentNode; //neste caso o TR
-    paiDoAlvo.remove();
+
+    paiDoAlvo.classList.add("fadeOut"); //leva a opacidade 0 em meio segundo, ver no css
+    
+    setTimeout(function () {
+        paiDoAlvo.remove();
+    }, 500); //espera meio segundo pra remover
 })
 
-/*
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        console.log("Fui clicado");
-        this.remove(); //this: a quem o evento está atrelado
-    })
-});*/
+
